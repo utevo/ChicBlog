88361b79c4282ecae766f656301afae356a33fc9
Michał Kowieski <kowieski.m98@gmail.com>
2019-09-18 21:48:57 +0200

    Rename one field in chickblog:Post model.

--- chicblog/models.py
+++ chicblog/models.py
@@ -5,14 +5,12 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField()
-    data_posted = models.TimeField(default=timezone.now)
+    date_posted = models.TimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
 
     class Meta:
-        db_table = ''
-        managed = True
         verbose_name = 'Post'
         verbose_name_plural = 'Posts'
