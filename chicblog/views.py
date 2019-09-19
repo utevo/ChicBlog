c11b802c26914d0462bb8e3ae599728a6143ea26
Michał Kowieski <kowieski.m98@gmail.com>
2019-09-19 20:52:25 +0200

    Change order of Posts in chicblog:home view.

--- chicblog/views.py
+++ chicblog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Post
 
 def home(request):
-    all_posts = Post.objects.all()
+    all_posts = Post.objects.order_by('-date_posted')
     context = { 
         'posts': all_posts
     }
