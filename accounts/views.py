d9170e39bf5d8f40c04cda327ba52b9263b7d1dc
Michał Kowieski <kowieski.m98@gmail.com>
2019-09-19 21:59:51 +0200

    Update chicblog:register view.

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
 
 def register(request):
-    form = UserCreationForm()
-    context = {
-        'form': form
-    }
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}')
+            return redirect('chicblog:home')
+    else:
+        form = UserCreationForm()
+
+    context = {'form': form }
     return render(request, 'accounts/register.html', context)
