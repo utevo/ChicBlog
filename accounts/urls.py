88a725b53f9c38f0c6ee7a02c351edf71c387bce
Michał Kowieski <kowieski.m98@gmail.com>
2019-09-22 21:22:38 +0200

    Add accounts:logout view.

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('register/', accounts_views.register, name='register' ),
     path('login/', auth_views.LoginView.as_view(
         template_name='accounts/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(
+        template_name='accounts/logout.html'), name='logout'),
 ]
