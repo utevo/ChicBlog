78802ec4ed1b866593a9f76c0f00290d811f8801
Michał Kowieski <kowieski.m98@gmail.com>
2019-09-22 21:14:33 +0200

    Add accounts:login view.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from . import views
+from django.contrib.auth import views as auth_views
+from . import views as accounts_views
 
 app_name = 'accounts'
 
 urlpatterns = [
-    path('register/', views.register, name='register' )
+    path('register/', accounts_views.register, name='register' ),
+    path('login/', auth_views.LoginView.as_view(
+        template_name='accounts/login.html'), name='login'),
 ]
