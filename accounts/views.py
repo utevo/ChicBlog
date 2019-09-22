9aee3f746523a04baf01179fa6647a11865f3127
Michał Kowieski <kowieski.m98@gmail.com>
2019-09-22 20:59:13 +0200

    Add email field to User registration form.

--- accounts/views.py
+++ accounts/views.py
@@ -1,17 +1,17 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.contrib.auth.forms import UserCreationForm
+from .forms import UserRegistrationForm
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegistrationForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}')
             return redirect('chicblog:home')
     else:
-        form = UserCreationForm()
+        form = UserRegistrationForm()
 
     context = {'form': form }
     return render(request, 'accounts/register.html', context)
