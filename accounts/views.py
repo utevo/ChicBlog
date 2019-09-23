88dd3809cfd3939ad4378ffcd1e141197e443148
Michał Kowieski <kowieski.m98@gmail.com>
2019-09-24 00:23:38 +0200

    Add 'accounts:profile' view.

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
 from .forms import UserRegistrationForm
+
 
 def register(request):
     if request.method == 'POST':
@@ -13,5 +16,10 @@ def register(request):
     else:
         form = UserRegistrationForm()
 
-    context = {'form': form }
+    context = {'form': form}
     return render(request, 'accounts/register.html', context)
+
+
+@login_required
+def profile(request):
+    return render(request, 'accounts/profile.html')
