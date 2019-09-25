b153b34032e866fea9fa9902a9db6baeb849c793
Michał Kowieski <kowieski.m98@gmail.com>
2019-09-25 22:52:29 +0200

    Update accounts:profile view.

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
-from .forms import UserRegistrationForm
+from .forms import (
+    UserRegistrationForm,
+    UserUpdateForm,
+    ProfileUpdateForm
+)
 
 
 def register(request):
@@ -22,4 +26,19 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request, 'accounts/profile.html')
+    if request.method == "POST":
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(
+            request.POST, request.FILES, instance=request.user.profile)
+        if user_form.is_valid and profile_form.is_valid:
+            user_form.save()
+            profile_form.save()
+            messages.success(request, "Your Profile has been updated.")
+            return redirect('accounts:profile')
+    else:
+        user_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {'user_form': user_form, 'profile_form': profile_form}
+
+    return render(request, 'accounts/profile.html', context)
