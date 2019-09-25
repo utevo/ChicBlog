9bcc45e9856896bcc38dbec675cc5d36e2f9ad94
Michał Kowieski <kowieski.m98@gmail.com>
2019-09-25 20:48:41 +0200

    Update accouts:Profile model (change default).

--- accounts/models.py
+++ accounts/models.py
@@ -4,8 +4,8 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default_profile_pics.jpg', 
-                              upload_to='profile_pics')  
+    image = models.ImageField(default='profile_pics/default.jpg', 
+                              upload_to='profile_pics/')  
 
     def __str__(self):
         return f'{self.user.username} Profile'
