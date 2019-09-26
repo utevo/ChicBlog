c79f14818b0563eba8ec3d1fab6123f2d2bc16d8
Michał Kowieski <kowieski.m98@gmail.com>
2019-09-26 20:51:56 +0200

    Create 'chicblog:post_create' path/view/template.

--- chicblog/urls.py
+++ chicblog/urls.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 
 from . import views
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView
+    )
 
 
 app_name = 'chicblog'
@@ -9,5 +14,6 @@ app_name = 'chicblog'
 urlpatterns = [
     path('', views.PostListView.as_view(), name='home'),
     path('post/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
+    path('post/new/', PostCreateView.as_view(), name='post_create'),
     path('about/', views.about, name='about')
 ]
--- chicblog/views.py
+++ chicblog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import (
+    ListView, 
+    DetailView, 
+    CreateView,
+    )
 
 from .models import Post
 
@@ -15,6 +19,16 @@ class PostDetailView(DetailView):
     model = Post
     template_name = 'chicblog/post_detail.html'
     context_object_name = 'post'
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'chicblog/post_create.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user;
+        return super().form_valid(form)
 
 
 def about(request):
