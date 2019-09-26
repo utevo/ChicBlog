b0c8c0c1a50db739c5c631b5618bb711414d3285
Michał Kowieski <kowieski.m98@gmail.com>
2019-09-26 20:01:18 +0200

    Change chicblog:home view from functional base view to generic class base view.

--- chicblog/urls.py
+++ chicblog/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+
 from . import views
 
 
 app_name = 'chicblog'
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.PostListView.as_view(), name='home'),
     path('about/', views.about, name='about')
 ]
--- chicblog/views.py
+++ chicblog/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+
 from .models import Post
 
-def home(request):
-    all_posts = Post.objects.order_by('-date_posted')
-    context = { 
-        'posts': all_posts
-    }
-    return render(request, 'chicblog/home.html', context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'chicblog/post_list.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
 
 
 def about(request):
     return render(request, 'chicblog/about.html')
-
