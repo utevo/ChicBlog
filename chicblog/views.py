e9bc86846167b858827e5f7e396987e33ee9d08f
Michał Kowieski <kowieski.m98@gmail.com>
2019-10-03 15:28:47 +0200

    Update paginator in 'chicblog:post_list' url.

--- chicblog/views.py
+++ chicblog/views.py
@@ -19,7 +19,7 @@ class PostListView(ListView):
     template_name = 'chicblog/post_list.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
-    paginate_by = 3
+    paginate_by = 6
 
 
 class PostDetailView(DetailView):
