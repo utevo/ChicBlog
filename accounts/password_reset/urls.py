721e5924ac98f3561b36393fa1ec5b5b1b0d2e39
Michał Kowieski <kowieski.m98@gmail.com>
2019-10-04 15:25:47 +0200

    Change in 'accounts:password_reset' urls.

--- accounts/password_reset/urls.py
+++ accounts/password_reset/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 
 
+app_name = 'password_reset'
+
 urlpatterns = [
     path('',
          auth_views.PasswordResetView.as_view(
              template_name='accounts/password_reset/home.html'
          ),
-         name='password_reset'),
+         name='home'),
 ]
--- accounts/urls.py
+++ accounts/urls.py
@@ -7,9 +7,10 @@ app_name = 'accounts'
 urlpatterns = [
     path('register/', accounts_views.register, name='register' ),
     path('login/', auth_views.LoginView.as_view(
-        template_name='accounts/login.html'), name='login'),
+         template_name='accounts/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(
-        template_name='accounts/logout.html'), name='logout'),
+         template_name='accounts/logout.html'), name='logout'),
     path('profile/', accounts_views.profile, name='profile'),
-    path('password_reset/', include('accounts.password_reset.urls')),
+    path('password/reset', 
+         include('accounts.password_reset.urls')),
 ]
