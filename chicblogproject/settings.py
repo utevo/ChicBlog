80cadcc795c734ded254860e87cd57fcf37a546e
Michał Kowieski <kowieski.m98@gmail.com>
2019-10-04 16:26:31 +0200

    Add email settings into 'chicblogproject/settings.py' file.

--- chicblogproject/settings.py
+++ chicblogproject/settings.py
@@ -139,3 +139,11 @@ LOGIN_URL = 'accounts:login'
 # media settings
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
+
+# email settings
+EMAIL_BACKEDN = 'django.core.mail.backend.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('CHICBLOG_EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('CHICBLOG_EMAIL_HOST_PASSWORD')
