d7430e63b287eb77656f183f48af5017b773cc18
Michał Kowieski <kowieski.m98@gmail.com>
2019-10-07 19:32:32 +0200

    Change EMAIL_BACKEND in 'chicblogproject/settings.py'.

--- chicblogproject/settings.py
+++ chicblogproject/settings.py
@@ -141,7 +141,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
 
 # email settings
-EMAIL_BACKEDN = 'django.core.mail.backend.smtp.EmailBackend'
+EMAIL_BACKEDN = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
