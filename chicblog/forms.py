f5c90e78f74529bb9416ac3b358120a8973327f4
Michał Kowieski <kowieski.m98@gmail.com>
2019-10-08 20:36:42 +0200

    Change look of 'chicblog.forms.CommentCreateForm' form. vol. 2

--- chicblog/forms.py
+++ chicblog/forms.py
@@ -17,6 +17,9 @@ class CommentCreateForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['content']
+        labels = {
+            'content': ''
+        }
         widgets = {
             'content': forms.Textarea(attrs={'rows': 2})
         }
