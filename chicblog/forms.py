475555e5f5ab68c9941d9baeff4c947ccc845f85
Michał Kowieski <kowieski.m98@gmail.com>
2019-10-08 20:21:48 +0200

    Change look of 'chicblog.forms.CommentCreateForm' form.

--- chicblog/forms.py
+++ chicblog/forms.py
@@ -7,10 +7,16 @@ class CommentCreateForm(forms.ModelForm):
     def is_valid(self):
         comment = self.instance
         
-        if comment.author is None or comment.post is None:
+        if comment.author is None or comment.post is None: #ToDo: Change THIS!
             return False
         return super().is_valid()
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
     class Meta:
         model = Comment
         fields = ['content']
+        widgets = {
+            'content': forms.Textarea(attrs={'rows': 2})
+        }
